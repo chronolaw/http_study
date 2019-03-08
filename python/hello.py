ee1149fd98038fb6e426836c3375c68907cda7fe
chronolaw <chrono_cpp@me.com>
2019-03-08 12:59:11 +0800

    python

---
+++ python/hello.py
@@ -0,0 +1,16 @@
+#!python
+
+# Copyright (C) 2019 by chrono
+
+import httplib
+
+url = 'https://127.0.0.1/'
+
+conn = httplib.HTTPConnection("127.0.0.1")
+conn.request(method="GET", url=url)
+
+response = conn.getresponse()
+res = response.read()
+
+print res
+
