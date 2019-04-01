52d536f52443aba625f2f2192794193108910c0f
chronolaw <chrono_cpp@me.com>
2019-04-01 15:28:33 +0800

    09-1.lua

--- www/lua/http/09-1.lua
+++ www/lua/http/09-1.lua
@@ -1,10 +1,34 @@
 -- Copyright (C) 2019 by chrono
 
 local headers = ngx.req.get_headers()
+local raw_headers = ngx.req.raw_header()
+
+local str = {}
 
 for k,v in pairs(headers) do
-    ngx.say(k, "=>", v)
+    --ngx.say(k, "\t\t\t\t=>", v)
+    --ngx.say(string.format("%-10s=>", k))
+    local s = string.format("%-10s=>", k)
+
+    if type(v) == 'string' then
+        --ngx.say(v)
+        s = s .. v
+    else
+        --ngx.say(table.concat(v, ' '))
+        s = s .. table.concat(v, ',')
+    end
+
+    str[#str + 1] = s
 end
 
-ngx.say("raw header is :\n")
-ngx.say(ngx.req.raw_header())
+str[#str + 1] = "\nraw header is :\n"
+
+str = table.concat(str, '\n')
+
+ngx.header.content_length = #str + # raw_headers
+
+ngx.print(str)
+ngx.print(raw_headers)
+
+--ngx.say("\nraw header is :\n")
+--ngx.say(ngx.req.raw_header())
