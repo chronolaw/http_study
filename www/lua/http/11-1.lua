6c3d0fdc457a98d8fe3034bc02a819f12b6e2e20
chronolaw <chrono_cpp@me.com>
2019-04-03 15:43:48 +0800

    11-1.lua

---
+++ www/lua/http/11-1.lua
@@ -0,0 +1,20 @@
+-- Copyright (C) 2019 by chrono
+
+local strs = {}
+
+strs[#strs + 1] = ngx.var.scheme .. "://" ..
+                  ngx.var.host .. ":" ..
+                  ngx.var.server_port ..
+                  ngx.var.request_uri .. '\n'
+
+strs[#strs + 1] = "scheme => " .. ngx.var.scheme
+strs[#strs + 1] = "host   => " .. ngx.var.host
+strs[#strs + 1] = "port   => " .. ngx.var.server_port
+strs[#strs + 1] = "path   => " .. ngx.var.uri
+strs[#strs + 1] = "query  => " .. (ngx.var.args or "")
+
+local str = table.concat(strs, '\n')
+
+ngx.header.content_length = #str
+
+ngx.print(str)
