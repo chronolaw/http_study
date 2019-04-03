59b2882b8834015840afddd35ebc227668b0da8e
chronolaw <chrono_cpp@me.com>
2019-04-03 17:07:12 +0800

    12-1.lua

--- www/lua/http/12-1.lua
+++ www/lua/http/12-1.lua
@@ -1,16 +1,18 @@
 -- Copyright (C) 2019 by chrono
 
-local code = ngx.var.arg_code
+local code = ngx.var.arg_code or "nil"
 
 --local str = "Your expect code is " ..
 --            (code or "nil") .. '\n'
 
 --ngx.header.content_length = #str
 
-ngx.header['Expect-Code'] = code or "nil"
+ngx.header['Expect-Code'] = code
 
 --ngx.status = tonumber(code) or 400
 
 --ngx.print(str)
 
-ngx.exit(tonumber(code) or 400)
+code = tonumber(code) or 400
+
+ngx.exit(code < 200 and 501 or code)
