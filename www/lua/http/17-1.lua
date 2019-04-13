5323ed545d8be41dc7f82782ed7d1cc9b90eed5c
chronolaw <chrono_cpp@me.com>
2019-04-13 19:34:56 +0800

    17-1.lua

--- www/lua/http/17-1.lua
+++ www/lua/http/17-1.lua
@@ -1,15 +1,20 @@
 -- Copyright (C) 2019 by chrono
 
-local target = ngx.var.arg_dst
+local target = ngx.var.arg_dst or '/index.html'
+
 local code = tonumber(ngx.var.arg_code or 302)
 
 if code ~= 301 and code ~= 302 then
     code = 302
 end
 
-local new_uri = target or 'index.html'
+local new_uri = target
+
+--if string.byte(new_uri) ~= string.byte('/') then
+--    new_uri = '/' .. new_uri
+--end
 
 ngx.header['Referer'] = ngx.var.request_uri
 
-ngx.redirect('/' .. new_uri, code)
+ngx.redirect(new_uri, code)
 
