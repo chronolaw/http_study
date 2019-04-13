f682bad7502e1dd0c3acdba6e7c2c634ad56093b
chronolaw <chrono_cpp@me.com>
2019-04-13 18:48:11 +0800

    16-1.lua

---
+++ www/lua/http/16-1.lua
@@ -0,0 +1,37 @@
+-- Copyright (C) 2019 by chrono
+
+local misc = ngx.shared.misc
+
+--local close = ngx.var.arg_close or '0'
+
+local counter = 1
+
+if misc then
+    counter = misc:incr("counter", 1, 0, 30)
+end
+
+local str = "keep alive data ".. counter .." \n"
+
+--if close == '1' then
+--    misc:set("counter", 0)
+--    ngx.header['Connection'] = 'close'
+--end
+
+ngx.header['Content-Length'] = #str
+ngx.header['Content-Type'] = 'text/plain'
+
+ngx.print(str)
+
+--local conn = ngx.header['Connection']
+--ngx.log(ngx.ERR, "conn = ", conn)
+--
+--if conn == 'close' then
+--    misc:set("counter", 0)
+--end
+
+--if close == '1' then
+--    ngx.flush(true)
+--
+--    local sock = ngx.req.socket(true)
+--    sock:close()
+--end
