8398094c8b8151f519b4c36d8259983bb5045613
chronolaw <chrono_cpp@me.com>
2019-04-17 11:54:26 +0800

    code clean

--- www/lua/17-1.lua
+++ www/lua/17-1.lua
@@ -1,6 +1,6 @@
 -- Copyright (C) 2019 by chrono
 
-local target = ngx.var.arg_dst or '/index.html'
+local dst = ngx.var.arg_dst or '/index.html'
 
 local code = tonumber(ngx.var.arg_code or 302)
 
@@ -8,7 +8,7 @@ if code ~= 301 and code ~= 302 then
     code = 302
 end
 
-local new_uri = target
+local new_uri = dst
 
 --if string.byte(new_uri) ~= string.byte('/') then
 --    new_uri = '/' .. new_uri
