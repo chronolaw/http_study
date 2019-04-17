5917f225013f91799f7e2f24b70b54595fce04fd
chronolaw <chrono_cpp@me.com>
2019-04-17 20:28:16 +0800

    15-1.lua

--- www/lua/15-1.lua
+++ www/lua/15-1.lua
@@ -1,9 +1,9 @@
 -- Copyright (C) 2019 by chrono
 
-local file = ngx.var.args
+local file = ngx.var.arg_name
 
 if not file then
-    local str = "You need use URI which looks like '/15-1?file'\n"
+    local str = "You need use URI which looks like '/15-1?name=file'\n"
 
     ngx.header['Content-Length'] = #str
     --ngx.header['Content-Type'] = 'text/plain'
