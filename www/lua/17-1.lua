8ad21480d36d04612606e813ffd83d734265887f
chronolaw <chrono_cpp@me.com>
2019-04-18 16:29:49 +0800

    17-1.lua

--- www/lua/17-1.lua
+++ www/lua/17-1.lua
@@ -8,7 +8,12 @@ if code ~= 301 and code ~= 302 then
     code = 302
 end
 
-local new_uri = dst
+local new_uri = ngx.escape_uri(dst)
+
+-- HTTP header Injection
+if string.find(new_uri, '\r') then
+    ngx.exit(400)
+end
 
 --if string.byte(new_uri) ~= string.byte('/') then
 --    new_uri = '/' .. new_uri
