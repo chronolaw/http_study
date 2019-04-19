bd01962a6f1b524f27bad22585b69c8fb82d6510
chronolaw <chrono_cpp@me.com>
2019-04-19 22:04:41 +0800

    16-3.lua

---
+++ www/lua/16-3.lua
@@ -0,0 +1,56 @@
+-- Copyright (C) 2019 by chrono
+
+-- chunked range
+
+--[[
+GET /16-2 HTTP/1.1
+Host: www.chrono.com
+Range: bytes=0-9, 20-29
+
+--xyz
+Content-Type: text/plain
+Content-Range: bytes 0-9/90
+
+// this is
+--xyz
+Content-Type: text/plain
+Content-Range: bytes 20-29/90
+
+ext json d
+--xyz--
+--]]
+
+--ngx.header['Content-Type'] = 'text/plain'
+--ngx.header['Transfer-Encoding'] = 'chunked'
+
+ngx.header['Accept-Ranges'] = 'bytes'
+ngx.header['Content-Type'] = 'multipart/byteranges; boundary=xyz'
+
+local strs = {
+[[
+--xyz
+Content-Type: text/plain
+Content-Range: bytes 0-9/90
+
+// this is
+]],
+
+[[
+--xyz
+Content-Type: text/plain
+Content-Range: bytes 20-29/90
+
+ext json d
+]],
+
+[[
+--xyz--
+]]
+    }
+
+-- flush for chunked
+for _,v in ipairs(strs) do
+    ngx.print(v)
+    ngx.flush(true)
+end
+
