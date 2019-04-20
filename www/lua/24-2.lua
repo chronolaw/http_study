8dd520b442af19f07510b819b42cb9fd06054d78
chronolaw <chrono_cpp@me.com>
2019-04-20 13:20:38 +0800

    24-2.lua

---
+++ www/lua/24-2.lua
@@ -0,0 +1,63 @@
+-- Copyright (C) 2019 by chrono
+
+local resty_rsa = require "resty.rsa"
+local resty_str = require "resty.string"
+
+local scheme = ngx.var.scheme
+if scheme ~= 'https' then
+    --ngx.log(ngx.ERR, scheme)
+    return ngx.redirect(
+        'https://'..ngx.var.host..ngx.var.request_uri, 301)
+end
+
+local algo_name = ngx.var.arg_algo or 'sha1'
+
+local algo = require('resty.' .. algo_name)
+
+if not algo then
+    ngx.say('no algorithm: ', algo_name)
+    return ngx.exit(400)
+end
+
+local rsa_public_key = [[
+-----BEGIN RSA PUBLIC KEY-----
+MIGJAoGBALRafyXmEY9wBw/lvedQIjP8ZYPEY45S9pqOGYNyQoXAOVEQIMSv5eo2
+rgWFREdp2tw25PDjL6+KF3D7sAPUI1j/Nyxq17xcUrFHskKyNnMKJpxHTDrrZmFD
+GDj2oWw4kwRL9+m8mhpcFB0qkkJ66q3eb9bqJA7frZxsCRPDW52VAgMBAAE=
+-----END RSA PUBLIC KEY-----
+]]
+
+local rsa_priv_key =[[
+-----BEGIN RSA PRIVATE KEY-----
+MIICXwIBAAKBgQC0Wn8l5hGPcAcP5b3nUCIz/GWDxGOOUvaajhmDckKFwDlRECDE
+r+XqNq4FhURHadrcNuTw4y+vihdw+7AD1CNY/zcsate8XFKxR7JCsjZzCiacR0w6
+62ZhQxg49qFsOJMES/fpvJoaXBQdKpJCeuqt3m/W6iQO362cbAkTw1udlQIDAQAB
+AoGBAKpzuSWlakVJWLNSq4dZeenuCjddvcW+bSknUb+klnB4evM9LesWX1JbeV7o
+U962kc186CUuYlwiRANZLEKCFSCqQ50KxotT3lZWdcvcQTh625hIPQAPJ5L3UGjx
+I1er83KmDeoxk07wNAjmYrTnYXrRxaknJd6/65ke4XeQarBBAkEA3YJ5zvI+sJTp
+JrkKnm9U/kZRMcM0QRQLw2iMR58vXmgl+xSOHdtaHs3fylq/xhCh5HlEUeqOrYmN
+G6Ci6p+IMQJBANBvgiN1rHKT7M140rEvwIKJf2W+wU2Sf/VkJS6OE+eGb0tzZTYD
+s4g3QLFnqPQrUsZ94NGFi8tQ8fJKbsOWtqUCQQDL0pNi6WTl9x/SkdJDlw4OK4Xq
+1EPw3hE07a6m+MMNi6fnMTLUJlL2pVmXSYnNJuDQ6wUCm2JOLJO7KETAv6sBAkEA
+orUZGsMmHb8ZkH/rwMMs/PmGiI8y6HIfDxjg6YmhQg+wW262KEcVY5T2HEZ2Hjyf
+fjEPSZ99M/Z5GBFAi8/fvQJBAIMGwpXeDRi2GPhxdql1YEh8fanCq0Rz4teee6+m
+emH+NTGnX6plyikqghnE8RAoR9TMsXR9Eg/KWvblxXS8/V4=
+-----END RSA PRIVATE KEY-----
+]]
+
+local plain = ngx.var.arg_text or '1234'
+
+local priv, err = resty_rsa:new(
+    { private_key = rsa_priv_key, algorithm = algo_name })
+local pub, err = resty_rsa:new(
+    { public_key = rsa_public_key, algorithm = algo_name })
+
+local sig, err = priv:sign(plain)
+
+
+
+ngx.say('algo  : rsa1024 with ', algo_name)
+ngx.say('plain : ', plain)
+ngx.say('signature: ', resty_str.to_hex(sig))
+ngx.say('verify   : ', pub:verify(plain, sig))
+
