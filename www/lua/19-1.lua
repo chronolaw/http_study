789db3070528900e6836deb38d3ecb67940f4a68
chronolaw <chrono_cpp@me.com>
2019-04-22 16:03:32 +0800

    19-1.lua

--- www/lua/19-1.lua
+++ www/lua/19-1.lua
@@ -9,15 +9,29 @@ if cookie then
     return
 end
 
+local max_age = 10
+
+local fields = {
+    'uid=2019-9999',
+    'Max-age=' .. max_age,
+    'expires=' ..  ngx.cookie_time(ngx.time() + max_age),
+    'domain=' ..  ngx.var.host,
+    'path=/',
+    'HttpOnly'
+    }
+
+ngx.header['Set-Cookie'] = table.concat(fields, '; ')
+
+--[[
 ngx.header['Set-Cookie'] = 'uid=2019-9999; ' ..
-                           'path=/; ' ..
                            'expires=' ..
                              ngx.cookie_time(ngx.time() + 10) ..
                              ';' ..
                            'domain=' ..
                              ngx.var.host .. '; ' ..
+                           'path=/; ' ..
                            'HttpOnly'
+--]]
 
 ngx.say("your have no cookie, please visit again. ")
-
 
