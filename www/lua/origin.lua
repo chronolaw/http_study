8589a125374436e2632bf672a45754701a529c31
chronolaw <chrono_cpp@me.com>
2019-04-24 21:42:15 +0800

    proxy

--- www/lua/origin.lua
+++ www/lua/origin.lua
@@ -7,7 +7,7 @@ local time = tonumber(misc:get(key))
 
 if not time then
     time = ngx.time()
-    misc:set(key, time, 10)   -- seconds
+    misc:set(key, time, 30)   -- seconds
 end
 
 local str = "HTTP Original Server\n" ..
@@ -23,15 +23,15 @@ for _,v in ipairs(fields) do
 end
 
 ngx.header['Content-Length'] = #str
-ngx.header['X-Powered-By'] = 'ngx_lua' .. ngx.config.ngx_lua_version
+ngx.header['X-Powered-By'] = 'ngx_lua_' .. ngx.config.ngx_lua_version
 --ngx.header['Content-Type'] = 'text/plain'
 
 ngx.header['Origin'] = ngx.var.scheme .. "://" ..
                        ngx.var.host .. ":" ..
                        ngx.var.server_port
 
-ngx.header['Cache-Control'] = 'public, max-age=10'
-ngx.header['Expires'] = ngx.http_time(time + 10)
+ngx.header['Cache-Control'] = 'public, max-age=10, s-maxage=30'
+--ngx.header['Expires'] = ngx.http_time(time + 10)
 
 ngx.header['Last-Modified'] = ngx.http_time(time)
 ngx.header['ETag'] = string.format('"%x-%x"', time, #str)
