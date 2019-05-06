a6cdf831051f170e1d3f1ae1ef87a27d6706b71c
chronolaw <chrono_cpp@me.com>
2019-05-06 16:01:29 +0800

    23-1.lua

--- www/lua/23-1.lua
+++ www/lua/23-1.lua
@@ -36,6 +36,11 @@ if scheme ~= 'https' then
         'https://'..ngx.var.host..ngx.var.request_uri, 301)
 end
 
+-- workaround on ubuntu 1604 OpenSSL 1.0.1f
+if not pcall(ffi.typeof, 'OpenSSL_version') then
+    return ngx.say('read openssl error.')
+end
+
 local openssl_ver = ffi_str(C.OpenSSL_version(0))
 
 ngx.log(ngx.INFO, "hello openssl")
