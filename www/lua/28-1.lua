f1f1d487678b84f9c8697f41597fb619ca9ee48f
chronolaw <chrono_cpp@me.com>
2019-05-20 10:55:07 +0800

    28-1.lua

--- www/lua/28-1.lua
+++ www/lua/28-1.lua
@@ -13,7 +13,7 @@ end
 local strs = {}
 
 strs[#strs + 1] = 'ssl handshake by ' ..  ssl.get_tls1_version_str()
-strs[#strs + 1] = 'ssl session id is [' .. ngx.var.ssl_session_id .. ']'
+strs[#strs + 1] = 'ssl session id is [' .. (ngx.var.ssl_session_id or '') .. ']'
 strs[#strs + 1] = 'reused? ' .. (ngx.var.ssl_session_reused == 'r' and 'true' or 'false')
 
 local str = table.concat(strs, '\n\n')
