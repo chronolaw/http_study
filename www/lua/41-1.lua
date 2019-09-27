3dd796be2a7cdeaa071995ca8e2df487e48e07f4
chronolaw <chrono_cpp@me.com>
2019-09-27 16:34:13 +0800

    dh algo

--- www/lua/41-1.lua
+++ www/lua/41-1.lua
@@ -7,6 +7,7 @@ ngx.say('DH demo\n')
 local P = 17
 local G = 5
 
+ngx.say("public:")
 ngx.say("P = ", P)
 ngx.say("G = ", G)
 ngx.say("")
@@ -14,6 +15,7 @@ ngx.say("")
 local a = 10
 local b = 5
 
+ngx.say("private:")
 ngx.say("a = ", a)
 ngx.say("b = ", b)
 ngx.say("")
@@ -21,12 +23,15 @@ ngx.say("")
 local A = (G ^ a) % P
 local B = (G ^ b) % P
 
-ngx.say("A = ", A)
-ngx.say("B = ", B)
+ngx.say("public:")
+ngx.say("A = (G ^ a % P) = ", A)
+ngx.say("B = (G ^ b % P) = ", B)
 ngx.say("")
 
 local shared_a = (B ^ a) % P
 local shared_b = (A ^ b) % P
 
-ngx.say("shared_a = ", shared_a)
-ngx.say("shared_b = ", shared_b)
+ngx.say("private:")
+ngx.say("shared_a = (B ^ a % P) = ", shared_a)
+ngx.say("shared_b = (A ^ b % P) = ", shared_b)
+ngx.say("B ^ a = (G ^ b ) ^ a = (G ^ a) ^ b = A ^ b (ignore 'mod P')")
